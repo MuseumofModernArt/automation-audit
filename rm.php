9cfaa1759507b6758c942bf2db5ed25cba598d7c
Ben Fino-Radin <bfinoradin@gmail.com>
2015-05-11 15:48:00 -0400

    fix php

--- rm.php
+++ rm.php
@@ -3,14 +3,10 @@
 if (isset($_POST['delete'])){
 	transfer_rm($_POST['delete']);
 	$uuid = $_POST['delete'];
-}
-
-date_default_timezone_set('America/New_York');
-$datedel = date('m/d/Y h:i:s a', time());
-
-function transfer_rm($id){
+	date_default_timezone_set('America/New_York');
+	$datedel = date('m/d/Y h:i:s a', time());
 	$db = new SQLite3('transfers.db');
-	$db->exec('UPDATE unit SET dateDeletedgood="$datedel" WHERE uuid="$uuid"');
+	$db->exec('UPDATE unit SET dateDeletedgood=$datedel WHERE uuid="$uuid"');
 }
 
 
