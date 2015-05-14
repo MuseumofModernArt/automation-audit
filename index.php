df8ee33bf7c6986781af50b2f1484a3d27415d1a
Ben Fino-Radin <bfinoradin@gmail.com>
2015-05-14 15:39:13 -0400

    jq before bs

--- index.php
+++ index.php
@@ -58,9 +58,8 @@ transfers.db "unit" table columns
 
 	<!-- Optional theme -->
 	<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.4/css/bootstrap-theme.min.css">
-
-	<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.4/js/bootstrap.min.js"></script>
 	<script src="https://code.jquery.com/jquery-1.11.3.min.js"></script>
+	<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.4/js/bootstrap.min.js"></script>
 
 
 	<style type="text/css">
