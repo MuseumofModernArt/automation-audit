d2407e932f2f65b5d7cf51ca7d2bad4a6c1185b4
Ben Fino-Radin <bfinoradin@gmail.com>
2015-05-14 15:14:26 -0400

    bootstrap js

--- index.php
+++ index.php
@@ -64,7 +64,9 @@ transfers.db "unit" table columns
 	<!-- Optional theme -->
 	<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.4/css/bootstrap-theme.min.css">
 
+	<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.4/js/bootstrap.min.js"></script>
 	<script src="https://code.jquery.com/jquery-1.11.3.min.js"></script>
+
 
 	<style type="text/css">
 		body { padding-top: 70px; }
