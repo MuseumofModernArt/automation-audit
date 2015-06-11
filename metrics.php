a996086baa5837645ce09f3d4ed63b962d2f2c49
Ben Fino-Radin <bfinoradin@gmail.com>
2015-06-11 16:25:28 -0400

    d3 initial

--- metrics.php
+++ metrics.php
@@ -75,7 +75,7 @@
 		$run_component = $row[2];
 		$readyForIngest = $row[3];
 		$artworkBacklog = $row[4];
-		echo $row;
+		echo var_dump($row);
 		// add these to the JSON for the D3 chart
 
 	};
@@ -85,7 +85,29 @@
 	
 ?>
 
+<svg id="visualisation" width="1000" height="500"></svg>
+
 </body>
 
+<script type="text/javascript">
+var vis = d3.select("#visualisation"),
+    WIDTH = 1000,
+    HEIGHT = 500,
+    MARGINS = {
+        top: 20,
+        right: 20,
+        bottom: 20,
+        left: 50
+    },
 
+    xScale = d3.scale.linear().range([MARGINS.left, WIDTH - MARGINS.right]).domain([2000,2010]),
+    yScale = d3.scale.linear().range([HEIGHT - MARGINS.top, MARGINS.bottom]).domain([134,215]),
+    xAxis = d3.svg.axis()
+    .scale(xScale),
+  
+yAxis = d3.svg.axis()
+    .scale(yScale);
+    vis.append("svg:g")
+    .call(xAxis);
+</script>
 
