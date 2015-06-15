7024793bfa99be5434c0c75b472332c79aada04f
Ben Fino-Radin <bfinoradin@gmail.com>
2015-06-15 11:46:18 -0400

    change y max to 800

--- metrics.php
+++ metrics.php
@@ -221,7 +221,7 @@ var	svg = d3.select("body")
 
 	// Scale the range of the data
 	x.domain(d3.extent(pre_ingest_data, function(d) { return d.date; }));
-	y.domain([0, 1000]);
+	y.domain([0, 800]);
  
 
 
