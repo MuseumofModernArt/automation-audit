30dcf0a8b1b8b76e020c4b683e2d8e45c3f4f90c
Ben Fino-Radin <bfinoradin@gmail.com>
2016-01-29 12:00:50 -0500

    js 100%

--- metrics.php
+++ metrics.php
@@ -138,8 +138,8 @@ path {
 // set dimensions of the graph
 
 var margin = { top: 30, right: 20, bottom: 30, left: 50 },
-    width = '100%',
-    height = '100%';
+    width = window.innerWidth - margin.left - margin.right,
+    height = window.innerHeight - margin.top - margin.bottom;
 
 // parse the date format
 var	parseDate = d3.time.format("%Y-%m-%d").parse;
