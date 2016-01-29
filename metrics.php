6e8e1aeef32b5ab14c9714f6ee0e81d1c855f79a
Ben Fino-Radin <bfinoradin@gmail.com>
2016-01-29 12:07:49 -0500

    100

--- metrics.php
+++ metrics.php
@@ -173,8 +173,8 @@ var	valueline = d3.svg.line()
 // Adds the svg canvas
 var	svg = d3.select("body")
 	.append("svg")
-		.attr("width", width + margin.left + margin.right)
-		.attr("height", height + margin.top + margin.bottom)
+		.attr("width", '100%')
+		.attr("height", '100%')
 	.append("g")
 		.attr("transform", "translate(" + margin.left + "," + margin.top + ")");
  
