4eb04b31c702327a5fd8b8b3985e5e7cc7a1f3c4
Ben Fino-Radin <bfinoradin@gmail.com>
2016-02-12 14:05:58 -0500

    disable button when clicked

--- namer/index.php
+++ namer/index.php
@@ -91,7 +91,7 @@
 
 		}
 
-		print "<a class='btn btn-default' href='download.php?dirname=".$dirname."' type='submit'>Download folders</a><br><br>";
+		print "<a class='btn btn-default downloadbtn' href='download.php?dirname=".$dirname."' type='submit'>Download folders</a><br><br>";
 
 		print "<div class='well'><h1><span class='glyphicon glyphicon-folder-open' aria-hidden='true'></span>&nbsp;&nbsp;".$dirname."</h1></div>";
 
@@ -163,4 +163,11 @@
 	?>
 
 </div>
+
+
+<script type="text/javascript">
+$('.downloadbtn').click(function() {
+	$('button').prop('disabled', true);
+});
+</script>
 </body>
