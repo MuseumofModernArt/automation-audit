699793aa1fad16468f86348986a659877683b62b
Ben Fino-Radin <bfinoradin@gmail.com>
2016-02-12 14:02:44 -0500

    spacing

--- namer/index.php
+++ namer/index.php
@@ -91,7 +91,9 @@
 
 		}
 
-		print "<a class='btn btn-default' href='download.php?dirname=".$dirname."' type='submit'>Download folders</a><div class='well'><h1><span class='glyphicon glyphicon-folder-open' aria-hidden='true'></span>&nbsp;&nbsp;".$dirname."</h1></div>";
+		print "<a class='btn btn-default' href='download.php?dirname=".$dirname."' type='submit'>Download folders</a><br>";
+
+		print "<div class='well'><h1><span class='glyphicon glyphicon-folder-open' aria-hidden='true'></span>&nbsp;&nbsp;".$dirname."</h1></div>";
 
 		
 		foreach ($components as $component) {
