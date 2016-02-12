9ea73802bc573794cc57183057343162e3d8ec20
Ben Fino-Radin <bfinoradin@gmail.com>
2016-02-12 13:41:58 -0500

    trying new zip function

--- namer/index.php
+++ namer/index.php
@@ -105,38 +105,52 @@
 		}
 
 
-		// Get real path for our folder
-		$rootPath = realpath("/var/www/automation-audit/namer/".$dirname."/");
-
-		print $rootPath;
-
-		// Initialize archive object
-		$zip = new ZipArchive();
-		$zip->open("/var/www/automation-audit/namer/".$objectnum.".zip", ZipArchive::CREATE | ZipArchive::OVERWRITE);
-
-		// Create recursive directory iterator
-		/** @var SplFileInfo[] $files */
-		$files = new RecursiveIteratorIterator(
-		    new RecursiveDirectoryIterator($rootPath),
-		    RecursiveIteratorIterator::LEAVES_ONLY
-		);
-
-		foreach ($files as $name => $file)
+		function Zip($source, $destination)
 		{
-		    // Skip directories (they would be added automatically)
-		    if (!$file->isDir())
-		    {
-		        // Get real and relative path for current file
-		        $filePath = $file->getRealPath();
-		        $relativePath = substr($filePath, strlen($rootPath) + 1);
-
-		        // Add current file to archive
-		        $zip->addFile($filePath, $relativePath);
+		    if (!extension_loaded('zip') || !file_exists($source)) {
+		        return false;
 		    }
+
+		    $zip = new ZipArchive();
+		    if (!$zip->open($destination, ZIPARCHIVE::CREATE)) {
+		        return false;
+		    }
+
+		    $source = str_replace('\\', '/', realpath($source));
+
+		    if (is_dir($source) === true)
+		    {
+		        $files = new RecursiveIteratorIterator(new RecursiveDirectoryIterator($source), RecursiveIteratorIterator::SELF_FIRST);
+
+		        foreach ($files as $file)
+		        {
+		            $file = str_replace('\\', '/', $file);
+
+		            // Ignore "." and ".." folders
+		            if( in_array(substr($file, strrpos($file, '/')+1), array('.', '..')) )
+		                continue;
+
+		            $file = realpath($file);
+
+		            if (is_dir($file) === true)
+		            {
+		                $zip->addEmptyDir(str_replace($source . '/', '', $file . '/'));
+		            }
+		            else if (is_file($file) === true)
+		            {
+		                $zip->addFile($file, str_replace($source . '/', '', $file));
+		            }
+		        }
+		    }
+		    else if (is_file($source) === true)
+		    {
+		        $zip->addFile($file, str_replace($source . '/', '', $file));
+		    }
+
+		    return $zip->close();
 		}
 
-		// Zip archive will be created only after closing object
-		$zip->close();
+		Zip("/var/www/automation-audit/namer/".$dirname, './compressed.zip')
 
 		
 	}
