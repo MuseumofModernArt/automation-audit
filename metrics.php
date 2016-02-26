43191b29bd5a8ce61f63668c88a67a77c854cd78
Ben Fino-Radin <bfinoradin@gmail.com>
2016-02-26 10:04:13 -0500

    fixed margins

--- metrics.php
+++ metrics.php
@@ -1,6 +1,6 @@
 <!DOCTYPE html>
 <html lang="en">
-  <head>
+  <head> 
     <meta charset="utf-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1">
@@ -113,8 +113,8 @@ var	valueline = d3.svg.line()
 // Adds the svg canvas
 var	svg = d3.select("body")
 	.append("svg")
-		.attr("width", width)
-		.attr("height", height)
+		.attr("width", width + margin.left + margin.right)
+		.attr("height", height + margin.top + margin.bottom)
 	.append("g")
 		.attr("transform", "translate(" + margin.left + "," + margin.top + ")");
  
