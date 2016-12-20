ef3f455466816f75c8c9a4983f9f22955f547764
Ben Fino-Radin <bfinoradin@gmail.com>
2016-12-20 17:03:08 -0500

    latest

--- AIP-migration-log.py
+++ AIP-migration-log.py
@@ -2,7 +2,7 @@
 
 # for monitoring AIP store migration
 
-import json, urllib2
+import json, urllib2, datetime, csv, os
 
 API_root='http://archivematica.museum.moma.org:8000'
 rest_of_url = '/api/v2/file/?username=binder&api_key=38e381ab1aa8035deb97d28af4395985aedebed6&limit=2&offset=0&format=json'
@@ -18,12 +18,17 @@ package_count = 0
 arkivum_count = 0
 vnx_count = 0
 
+i = datetime.datetime.now()
+now = i.isoformat()
+now = now[:-16]
+
+print now
 
 while next != None:	
 	object_list = request['objects']
 	for item in object_list:
-		print package_count, item['uuid'], item['current_location']
 		package_count = package_count+1
+		print package_count, item['uuid'], item['current_location']
 		if item['current_location'] == arkivum_production:
 			arkivum_count = arkivum_count+1
 		if item['current_location'] == vnx_AIP_store:
@@ -34,3 +39,14 @@ while next != None:
 	
 print 'number of AIPs in Arkivum: ', arkivum_count
 print 'number of AIPs in VNX: ', vnx_count
+
+
+with open ('AIP-migration.csv', 'rb') as csvfile:
+	c = csv.writer(open('AIP-migration-2.csv', 'wb'))
+	reader = csv.reader(csvfile, delimiter=',')
+	for row in reader:
+		if row[0] != now:
+			c.writerow(row)
+	c.writerow([now,arkivum_count,vnx_count])
+
+os.rename('AIP-migration-2.csv','AIP-migration.csv')
